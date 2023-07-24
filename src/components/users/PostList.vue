e6740dfef5ecc44247d7a79acb0b9f35a01a2303
Kim-Yun-Seo <rlagrace410@gmail.com>
2023-07-24 20:58:05 +0900

    fix: variable name

--- src/components/users/PostList.vue
+++ src/components/users/PostList.vue
@@ -10,14 +10,14 @@ import { // collection, query, getDocs, DocumentData
 import PostListItem from './PostListItem.vue'
 import { Post, getPosts } from 'src/models/Post'
 
-const items = ref<QueryDocumentSnapshot<Post>[]>([])
+const posts = ref<QueryDocumentSnapshot<Post>[]>([])
 
 const getData = async () => {
   // const q = query(collection(db, 'posts'))
   // const querySnapshot = await getDocs(q)
 
   const querySnapshot = await getPosts()
-  items.value = querySnapshot.docs
+  posts.value = querySnapshot.docs
 }
 
 onMounted(() => {
@@ -29,15 +29,15 @@ onMounted(() => {
 <template>
   <q-list>
     <PostListItem
-      v-for="item in items"
-      :key = "item.id"
-      :item = "item"
+      v-for="post in posts"
+      :key = "post.id"
+      :item = "post"
       @refresh="getData"
       />
       <!-- 자식에서 refresh를 전달한다고 선언해서 여기서 @refresh가 사용가능함 -->
       <!-- 여기서 부모는 emit한걸 듣는 귀를 열어줘야함. -->
   </q-list>
-  <q-page-sticky positon="bottom-right" :offset="[18,18]">
+  <q-page-sticky positon="bottom-right" :offset="[18,30]">
     <q-btn round color="pink" icon="mdi-pencil" to="/write"/>
   </q-page-sticky>
 </template>
